20676066b995f914d8d6878a6bee343ec45ea9f3
leoresler <leandroresler4@gmail.com>
2025-05-07 14:33:27 -0300

    correccion buscador

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
-import { ROUTES } from '../../const/routes'; // Ajusta la ruta si es distinta
-import { useNavigate } from 'react-router';
-import { useTranslation } from 'react-i18next';
+import { ROUTES } from "../../const/routes"; // Ajusta la ruta si es distinta
+import { useNavigate } from "react-router";
+import { useTranslation } from "react-i18next";
 
 const Buscador = ({ tours }) => {
   const [busqueda, setBusqueda] = useState("");
@@ -10,9 +10,7 @@ const Buscador = ({ tours }) => {
   const navegar = useNavigate();
 
   const navegarDetalledHandler = (tipo, id) => {
-    const ruta = ROUTES.detalle
-      .replace(':tipo?', tipo)
-      .replace(':id?', id);
+    const ruta = ROUTES.detalle.replace(":tipo?", tipo).replace(":id?", id);
     navegar(ruta);
   };
 
@@ -27,17 +25,16 @@ const Buscador = ({ tours }) => {
 
   const toursFiltradas = () => {
     const termino = busqueda.trim().toLowerCase();
-    return tours.filter((dato) =>
-      getTextoTraducido(dato.pais)?.toLowerCase().includes(termino) ||
-      dato.ciudad?.toLowerCase().includes(termino) ||
-      getTextoTraducido(dato.provincia)?.toLowerCase().includes(termino) ||
-      dato.lugares?.some(lugar => lugar.toLowerCase().includes(termino))
+    return tours.filter(
+      (dato) =>
+        getTextoTraducido(dato.pais)?.toLowerCase().includes(termino) ||
+        dato.ciudad?.toLowerCase().includes(termino) ||
+        getTextoTraducido(dato.provincia)?.toLowerCase().includes(termino) ||
+        dato.lugares?.some((lugar) => lugar.toLowerCase().includes(termino))
     );
   };
 
-  useEffect(() => {
-
-  }, [busqueda, tours]);
+  useEffect(() => {}, [busqueda, tours]);
 
   return (
     <>
@@ -48,47 +45,52 @@ const Buscador = ({ tours }) => {
         <input
           id="busqueda"
           type="text"
-          placeholder={t('enterTour')}
+          placeholder={t("enterTour")}
           value={busqueda}
           onChange={buscarTour}
           className="w-full max-w-lg px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-sky-500"
         />
       </div>
 
-      <div className="max-w-3xl space-y-0">
-        {busqueda && toursFiltradas().length > 0 ? (
-          toursFiltradas().map((tour, index) => (
-            <div
-              key={index}
-              className="p-4 rounded-lg shadow-sm hover:shadow-md transition"
-            >
-              <button onClick={() => navegarDetalledHandler(tour.tipo, tour.id)}>
-                <h3 className="font-semibold text-lg">
-                  {tour.tipo === 'internacional' ? (
-                    <>
-                      {getTextoTraducido(tour.pais) || ""}. - {tour.ciudad || ""}.
-                    </>
-                  ) : (
-                    <>
-                      {getTextoTraducido(tour.provincia) || ""}. - {" "}{tour.lugares?.map((lugar, i) => (
-                        <span key={i}>
-                          {lugar}
-                          {i < tour.lugares.length - 1 ? ", " : ""}
-                        </span>
-                      ))}.
-                    </>
-                  )}
-                </h3>
-              </button>
-            </div>
-          ))
-        ) : (
-          busqueda && (
-            <div className="p-4 rounded-lg shadow-sm hover:shadow-md transition">
-              <p className="text-gray-500 mt-2">{t('noMatches')}</p>
-            </div>
-          )
-        )}
+      <div className="max-w-3xl space-y-0 mx-auto text-center">
+        {busqueda && toursFiltradas().length > 0
+          ? toursFiltradas().map((tour, index) => (
+              <div
+                key={index}
+                className="flex justify-center"
+              >
+                <div className="p-4 rounded-lg shadow-sm hover:shadow-md transition w-full">
+                  <button
+                    onClick={() => navegarDetalledHandler(tour.tipo, tour.id)}
+                  >
+                    <h3 className="font-semibold text-lg">
+                      {tour.tipo === "internacional" ? (
+                        <>
+                          {getTextoTraducido(tour.pais) || ""}. -{" "}
+                          {tour.ciudad || ""}.
+                        </>
+                      ) : (
+                        <>
+                          {getTextoTraducido(tour.provincia) || ""}. -{" "}
+                          {tour.lugares?.map((lugar, i) => (
+                            <span key={i}>
+                              {lugar}
+                              {i < tour.lugares.length - 1 ? ", " : ""}
+                            </span>
+                          ))}
+                          .
+                        </>
+                      )}
+                    </h3>
+                  </button>
+                </div>
+              </div>
+            ))
+          : busqueda && (
+              <div className="p-4 rounded-lg shadow-sm hover:shadow-md transition">
+                <p className="text-gray-500 mt-2">{t("noMatches")}</p>
+              </div>
+            )}
       </div>
     </>
   );
